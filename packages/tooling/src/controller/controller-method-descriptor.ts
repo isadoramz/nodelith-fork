f6907c5ec24226209740419b6d2a15ada12e9222
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2023-09-24 13:15:31 -0700

    chore: remove tooling package

--- packages/tooling/src/controller/controller-method-descriptor.ts
+++
@@ -1,6 +0,0 @@
-import { Function } from '@core-fusion/context'
-import { ControllerMethodMetadata } from "./controller-method-metadata";
-
-export type ControllerMethodDescriptor = TypedPropertyDescriptor<
-  Function & { [ControllerMethodMetadata.METADATA_KEY]?: ControllerMethodMetadata }
->;
