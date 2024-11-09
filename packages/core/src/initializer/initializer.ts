494e10d411b20b55cd566cabfb855fbfd620c01d
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2024-11-08 20:50:58 -0800

    Implement static method to tell whether or not a class extends Initializer

--- packages/core/src/initializer/initializer.ts
+++ packages/core/src/initializer/initializer.ts
@@ -1,6 +1,40 @@
-import{ InitializationFunction } from './initialization-function'
+import * as Types from '@nodelith/types'
 
+/**
+ * Abstract class representing an initializer.
+ * 
+ * @template Result - The type returned after initialization
+ */
 export abstract class Initializer<Result extends Record<string, any> = Record<string, any>> {
-  abstract initialize: InitializationFunction<Result>
-  abstract terminate?(): Promise<void>
+
+  /**
+   * @description Checks if a given constructor class extends the Initializer class.
+   * @param {Types.Constructor} constructor - The constructor to check.
+   * @returns {boolean} - True if the constructor extends Initializer; otherwise, false.
+   */
+  static isExtendedBy(constructor: Types.Constructor): boolean {
+    const prototype = Object.getPrototypeOf(constructor.prototype);
+
+    if (!prototype || prototype === Function.prototype) {
+      return false;
+    }
+
+    if (prototype === Initializer.prototype) {
+      return true;
+    }
+
+    return Initializer.isExtendedBy({ prototype } as Types.Constructor);
+  }
+
+  /**
+   * @description Optional method to terminate the initializer.
+   * @returns {void | Promise<void>} - Returns nothing.
+   */
+  public terminate?(): void | Promise<void>
+
+  /**
+   * @description Abstract method to initialize the initializer with provided arguments.
+   * @returns {Result} - Returns nothing.
+   */
+  public abstract initialize(): Result | Promise<Result>
 }
