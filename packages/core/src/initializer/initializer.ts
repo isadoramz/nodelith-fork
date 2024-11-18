5414494725b2d2f9d20596081cbc37d522ea664d
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2024-11-17 21:56:28 -0800

    Improve Initializer::isExtendedBy so that it returns type guard

--- packages/core/src/initializer/initializer.ts
+++ packages/core/src/initializer/initializer.ts
@@ -8,22 +8,26 @@ import * as Types from '@nodelith/types'
 export abstract class Initializer<Result extends Record<string, any> = Record<string, any>> {
 
   /**
-   * @description Checks if a given constructor class extends the Initializer class.
-   * @param {Types.Constructor} constructor - The constructor to check.
-   * @returns {boolean} - True if the constructor extends Initializer; otherwise, false.
+   * @description Checks if a given unknown value extends the Initializer class.
+   * @param {any} value - The unknown value to be checked.
+   * @returns {boolean} - Whether it extends Initializer or not.
    */
-  static isExtendedBy(constructor: Types.Constructor): boolean {
-    const prototype = Object.getPrototypeOf(constructor.prototype);
-
-    if (!prototype || prototype === Function.prototype) {
+  static isExtendedBy(value: unknown): value is Types.Constructor<Initializer> {
+    if (typeof value !== 'function' || !value.prototype) {
       return false;
     }
-
+  
+    const prototype = Object.getPrototypeOf(value.prototype);
+  
+    if (!prototype || prototype === Object.prototype) {
+      return false;
+    }
+  
     if (prototype === Initializer.prototype) {
       return true;
     }
-
-    return Initializer.isExtendedBy({ prototype } as Types.Constructor);
+  
+    return this.isExtendedBy(Object.getPrototypeOf(value));
   }
 
   /**
